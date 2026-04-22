21080515eb7be034a47688de0a820c7afc3989cf
Edem <edem.quashigah@student.hamk.fi>
2026-04-22 16:43:39 +0300

    Connecting from app

--- bloglist/cli.js
+++ bloglist/cli.js
@@ -13,17 +13,18 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 
 const main = async () => {
   try {
+    // connect to database and retrieve blogs
     await sequelize.authenticate()
     console.log('Connection has been established successfully')
     const notes = await sequelize.query('SELECT * FROM blogs', {
       type: QueryTypes.SELECT,
     })
 
+    // Print the notes in readable format in the console
     notes.map((note) => {
       console.log(`${note.author}: '${note.title}', ${note.likes} likes`)
     })
 
-    // console.log(notes)
     sequelize.close()
   } catch (error) {
     console.error('Unable to connect to the database: ', error)

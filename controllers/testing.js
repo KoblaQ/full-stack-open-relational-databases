bc6ff731d8c9f5518cc5296ab88513ac01dd5b50
Edem <edem.quashigah@student.hamk.fi>
2026-04-27 17:45:21 +0300

    Checkup

---
+++ controllers/testing.js
@@ -0,0 +1,23 @@
+const router = require('express').Router()
+
+const { User, Blog } = require('../models')
+
+router.get('/', async (req, res) => {
+  return res.status(200).end()
+})
+
+router.post('/api/reset', async (req, res) => {
+  try {
+    await User.destroy({
+      truncate: true,
+    })
+    await Blog.destroy({
+      truncate: true,
+    })
+    return res.status(204).end()
+  } catch (error) {
+    return res.status(400).json(error)
+  }
+})
+
+module.exports = router

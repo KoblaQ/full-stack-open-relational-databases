e0f70f67166b1ed6241e0d4ada166975a67aef47
Edem <edem.quashigah@student.hamk.fi>
2026-04-29 19:42:39 +0300

    Reading list

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const Blog = require('./blog')
 const User = require('./user')
+const ReadingList = require('./readingList')
 
 // Make the relations
 User.hasMany(Blog)
@@ -8,7 +9,11 @@ Blog.belongsTo(User)
 // User.sync({ alter: true })
 // Blog.sync({ alter: true })
 
+User.belongsToMany(Blog, { through: ReadingList, as: 'read' })
+Blog.belongsToMany(User, { through: ReadingList, as: 'unread' })
+
 module.exports = {
   Blog,
   User,
+  ReadingList,
 }
